aa86a275fb7a416a5d51d9eb3d367d997c49f42c
Daniel Givens <daniel.givens@rackspace.com>
2014-10-09 14:05:37 -0500

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'cron-apt'
+maintainer       'Rackspace, Inc.'
+maintainer_email 'daniel.givens@rackspace.com'
+license          'Apache 2.0'
+description      'Installs/Configures cron-apt'
+long_description 'Installs/Configures cron-apt'
+version          '0.1.0'
+
